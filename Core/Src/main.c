25c6f7377362f81c29387580a4cf4ca81972bd83
Quence Z <qqq89513@yahoo.com.tw>
2020-12-28 00:54:12 +0800

    misc: Setup CMSIS DSP library include

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "arm_math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
