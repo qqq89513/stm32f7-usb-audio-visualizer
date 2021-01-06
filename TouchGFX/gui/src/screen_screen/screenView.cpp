1d366d3747b413cf52cfd7d21799b8d8c36254e0
Quence Z <qqq89513@yahoo.com.tw>
2021-01-06 15:37:48 +0800

    Fix #4 Display slows down when clicking button constantly by
    use the uwTick(the value of HAL_GetTick() returns) to replace tick_cnt.
    Cause: screenView::handleTickEvent() is not called every frame.
    So tick_cnt++ in screenView::handleTickEvent is not precice.

--- TouchGFX/gui/src/screen_screen/screenView.cpp
+++ TouchGFX/gui/src/screen_screen/screenView.cpp
@@ -1,6 +1,8 @@
 #include <gui/screen_screen/screenView.hpp>
 #include "math.h"
 
+extern uint32_t uwTick;
+
 screenView::screenView()
 {
   tick_cnt = 0;
@@ -17,8 +19,13 @@ void screenView::tearDownScreen()
 }
 
 void screenView::handleTickEvent(){
-  tick_cnt++;
-  if(tick_cnt%3 == 0){
-    graph_t.addDataPoint(sinf(tick_cnt*0.1));
+  static uint32_t tk_graph = 0;
+  static uint32_t tk_ctl = 0;
+  if(uwTick - tk_graph > 40){
+    tk_graph = uwTick;
+    graph_t.addDataPoint(sinf(uwTick*0.004));
+  }
+  if(uwTick - tk_ctl > 100){
+
   }
 }
